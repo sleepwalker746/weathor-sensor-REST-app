59b5fcc86a27ea784218a249aa98ed8f08ee5daf
sleepwalker746 <sleepwalker.8@proton.me>
2025-10-22 11:26:27 +0300

    Initial Commit

---
+++ src/main/java/ua/august/rest_weather_sensor/RestWeatherSensorApplication.java
@@ -0,0 +1,13 @@
+package ua.august.rest_weather_sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWeatherSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWeatherSensorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/august/rest_weather_sensor/RestWeatherSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.august.rest_weather_sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWeatherSensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
